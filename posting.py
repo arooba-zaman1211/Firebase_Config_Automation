3a01e6acf9d9387425e0f7e78396f93dac8b1021
Tehreem Farooqi <teefarooqi@gmail.com>
2024-09-05 17:30:08 +0500

    final

--- posting.py
+++ posting.py
@@ -10,22 +10,26 @@ load_dotenv()
 
 # Retrieve the access token and Instagram user ID
 access_token = os.getenv('ACCESS_TOKEN')
-ig_user_id = os.getenv('IG_ID')
+# ig_user_id = os.getenv('IG_ID')
 
-print(access_token, ig_user_id)
+print(access_token)
 
-response = requests.post(
-        f'https://graph.instagram.com/v20.0/{ig_user_id}/media',
-        headers={
-            'Content-Type': 'application/json'
-        },
-        data={
-            'image_url': 'https://raw.githubusercontent.com/TehreemFarooqi/whimnym/main/assets/images/nym_post.jpeg'
-        }
-    )
+# response = requests.post(
+#   f'https://graph.instagram.com/v20.0/{ig_user_id}/media',
+#   headers={
+#       'Content-Type': 'application/json'
+#   },
+#   data={
+#       'image_url': 'https://raw.githubusercontent.com/TehreemFarooqi/whimnym/main/assets/images/nym_post.jpeg'
+#   }
+# )
 
-try:
-    response_data = response.json()
-    print(response_data)
-except ValueError:
-    print("Response is not in JSON format:", response.text)
+me = requests.get(
+    'https://graph.facebook.com/v20.0/me',
+    params={
+    'fields': 'id',
+    'access_token': access_token
+    }
+)
+
+print(me.json())
